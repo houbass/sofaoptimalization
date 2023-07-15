62f62ea501a1b4cab0a08f1f10999034a644c594
houbass <laube.ondrej@seznam.cz>
2023-07-15 16:45:21 +0200

    asgg

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ export default function App({ Component, pageProps }) {
       </Head>
 
       <GlobalStatesProvider>
-        <Navbar />
+
         <Component {...pageProps} />
       </GlobalStatesProvider>
 
