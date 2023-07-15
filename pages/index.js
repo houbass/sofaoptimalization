c6ba68076502426766582e7e221afb72242fca91
houbass <laube.ondrej@seznam.cz>
2023-07-15 13:49:43 +0200

    yo

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,139 @@ export default function Home() {
 
   const [text, setText] = useState("");
 
-  //console.log(pageyoffset);
+    //refs
+    const topic1Ref = useRef();
+    const topic2Ref = useRef();
+    const topic3Ref = useRef();
+    const topic4Ref = useRef();
+  
+    //PASSING GLOBAL SETTINGS
+    const { setMainWidth, mobile } = useContext(GlobalStates);
+  
+    //MAIN PAGE
+    const mainRef = useRef(null);
+  
+    //SETTINGS
+    const maxWidth = 1300;
+    const [titleSize, setTitleSize] = useState("70px");
+  
+    //TOPIC 1 STATES
+    const [topic1Width, setTopic1Width] = useState("0");
+    const [div1Width, setDiv1Width] = useState(null);
+    const [topic1Opacity, setTopic1Opacity] = useState("0");
+    const [topic1blur, setTopic1blur] = useState("blur(5px)");
+    const [topic1Animation, setTopic1Animation] = useState(null);
+  
+    //TOPIC 2 STATES
+    const [topic2Width, setTopic2Width] = useState("90%");
+    const [topic2Opacity, setTopic2Opacity] = useState("0");
+    const [topic2blur, setTopic2blur] = useState("blur(5px)");
+    const [topic2Animation, setTopic2Animation] = useState(null);
+  
+    //TOPIC 3 STATES
+    const [topic3Width, setTopic3Width] = useState("90%");
+    const [topic3Animation, setTopic3Animation] = useState(null);
+  
+    //TOPIC 4 STATES
+    const [topic4Animation, setTopic4Animation] = useState(null);
+
+  //ZOBRAZOVANI ANIMACI KDYZ JE POTREBA
+  function scrollFun() {
+
+    const windowHeight = window.innerHeight;
+    const getscrollY = window.scrollY;
+    const constant = 200;
+
+
+  }
+
+  //WIDTH LESS THEN 850px and 700px
+  const [flexStyle1, setFlexStyle] = useState("row");
+  const [lottieWidth, setLottieWidth] = useState("300px");
+  const [pSize, setPsize] = useState("20px");
+
+  function resizeFun() {
+
+    //850px <
+    if(window.innerWidth < 850 ){
+      setTitleSize("50px");
+
+      //700px <
+      if(window.innerWidth < 700) {
+        setFlexStyle("column");
+        setLottieWidth("300px");
+        setPsize("20px");
+
+        //450px <
+        if(window.innerWidth < 450){
+          setTitleSize("35px");
+          setLottieWidth("190px");
+          setPsize("17px");
+
+
+          //320px <
+          if(window.innerWidth < 320){
+            setLottieWidth("150px");
+          }
+
+        }
+
+
+
+      }else{
+        setFlexStyle("row");
+        setLottieWidth("300px");
+        setPsize("20px");
+      }
+
+    }
+    if(window.innerWidth > 850 ){
+      setTitleSize("70px");
+    }
+
+    console.log(window.innerWidth)
+  }
+
+  //resize listener
+  useEffect(() => {
+    resizeFun();
+    window.addEventListener("resize", resizeFun);
+
+    return () => {
+      window.removeEventListener("resize", resizeFun);
+    }
+  }, [])
+
+
+  //scroll listener
+  useEffect(() => {
+    scrollFun();
+    window.addEventListener("scroll", scrollFun);
+
+    return () => {
+      window.removeEventListener("scroll", scrollFun);
+    }
+  }, [])
+
+
+  //ONLOAD ANIMATION
+  useEffect(() => {
+    setTopic1Width(`${div1Width}px`);
+
+    setTimeout(() => {
+      setTopic1Opacity("1");
+      setTopic1blur("blur(0px)");
+    }, 500);
+
+    setTimeout(() => {
+      setTopic2Opacity("1");
+      setTopic2blur("blur(0px)");
+    }, 1000);
+
+  }, [div1Width])
+
+
+
   return (
     <>
   <div>
