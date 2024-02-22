cbdd584c341c6765a79a31c18935349da3888b73
houbass <laube.ondrej@seznam.cz>
2024-02-22 16:30:41 +0100

    bug fix

--- config/apiKeys.js
+++ config/apiKeys.js
@@ -1,6 +1,9 @@
 export const apiKeys = {
-    google: process.env.NEXT_PUBLIC_SECOND_API_KEY,
-    gaId: process.env.NEXT_PUBLIC_GAID,
+    //google: process.env.NEXT_PUBLIC_SECOND_API_KEY,
+    //gaId: process.env.NEXT_PUBLIC_GAID,
+    google: "asadsd",
+    gaId: "asdasd1245",
+
     emailJs1: process.env.NEXT_PUBLIC_EMAIL1_KEY,
     emailJs2: process.env.NEXT_PUBLIC_EMAIL2_KEY,
     subscribe: process.env.NEXT_PUBLIC_API_KEY,
